56dc94a0c8c1afb176f7d47959f841a1ff8324c2
Jdespitia <juano.o@outlook.es>
2021-02-26 13:51:04 -0500

    upd control de hora, platform y opts para getGPS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Geolocation, GeolocationOptions } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +13,46 @@ export class HomePage {
   lat = 0;
   lon = 0;
 
-  constructor(private geolocation: Geolocation) { this.getGPS(); }
+  constructor(private geolocation: Geolocation, public platform: Platform) {
+    this.platform.ready().then((readySource) => {
+      console.log('Platform ready: ', readySource);
+      const options:GeolocationOptions = {
+        maximumAge: 0,
+        enableHighAccuracy: true
+      };
+      console.log(
+        '%cLlamando a getGPS, ' + this.mostrarhora(),
+        'background:#5924c1;color:white;font-size:15px'
+      );
+      this.getGPS(options);
+      });
+  }
 
 
-  getGPS(){
-    this.geolocation.getCurrentPosition().then((resp) => {
+  getGPS(options){
+    this.geolocation.getCurrentPosition(options).then((resp) => {
+      console.log(
+        '%cResponde GPS, ' + this.mostrarhora(),
+        'background:#5924c1;color:white;font-size:15px'
+      );
       this.lat = resp.coords.latitude;
       this.lon = resp.coords.longitude;
       console.log(resp);
       this.responde = true;
      }).catch((error) => {
+      console.log(
+        '%cError GPS, ' + this.mostrarhora(),
+        'background:#5924c1;color:red;font-size:15px'
+      );
        console.log('Error getting location', error);
      });
+  }
+
+  private mostrarhora(): string {
+    const f = new Date();
+    const cad = f.getHours() + ':' + f.getMinutes() + ':' + f.getSeconds();
+    window.status = cad;
+    return cad;
   }
   
 }
